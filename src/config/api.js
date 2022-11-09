02b692264342450b766d7fcd8c5bc401fd36815f
Bagas <mbagas221@gmail.com>
2022-11-09 23:31:42 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://waysgallery-app.herokuapp.com/",
+  baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
