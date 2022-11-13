d611d3784e41cd0a8213fd7d235da8b323a5950c
Bagas <mbagas221@gmail.com>
2022-11-13 21:10:03 +0700

    update

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -184,7 +184,6 @@ function Detail() {
           }
           style={{ width: "100%" }}
         ></img>
-        <a href={post?.image1} target="_blank"></a>
         <div className="d-flex justify-content-center mb-4">
           {post?.image2 ? (
             <img
